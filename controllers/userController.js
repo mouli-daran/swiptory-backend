ed3dc78a2ee768780a0c80fb9315b2255340b1b6
mouli-daran <moulidaran684@gmail.com>
2024-04-21 11:48:10 +0530

    updated user controller and route

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,3 +29,48 @@ exports.register = async (req, res, next) => {
     console.log(error);
   }
 };
+
+exports.logIn = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return next(new Error("Username and password are required"));
+    }
+
+    console.log(username, password);
+
+    const user = await User.findOne({ username }).select("+password");
+
+    if (!user) {
+      return res.status(409).json({
+        message: "User is not registered",
+      });
+    }
+
+    const isPasswordCorrect = await user.isValidatedPassword(password);
+
+    if (!isPasswordCorrect)
+      return next(new Error("You are password is wrong", 400));
+
+    cookieToken(user, res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.logOut = async (req, res, next) => {
+  try {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logout Success",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { register } = require("../controllers/userController");
+const { register, logIn, logOut } = require("../controllers/userController");
 
 router.route("/register").post(register);
+router.route("/login").post(logIn);
+router.route("/logout").get(logOut);
 
 module.exports = router;
